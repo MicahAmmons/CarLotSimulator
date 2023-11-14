f10bf4382b034c22156fe17796f39bfcf780297c
Micah Ammons <micahammons@gmail.com>
2023-11-14 15:34:56 -0500

    final commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public Car()
+        {
+        }
+        public Car(string make, int year, bool isDrivable, string model, string engineNoise, string honkNoise)
+        {
+           Make = make;
+            Year = year;
+            IsDrivable = isDrivable;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+              
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+        public void MakeEngineNoise(string EngineNoise)
+        {
+
+        }
+        public void MakeHonkNoise(string HonkNoise)
+        {
+
+        }
+
+        
+    }
+}
+
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -9,14 +9,45 @@ namespace CarLotSimulator
             //TODO
 
             //Create a seperate class file called Car
+
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
-
+            CarLot LottaCar = new CarLot();
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car car1 = new Car();
+                car1.Make = "Toyota";
+                car1.Year = 2020;
+                car1.IsDrivable = true;
+                car1.Model = "camry";
+                car1.EngineNoise = "Loud";
+                car1.HonkNoise = "Cute";
+            LottaCar.ListOfCars.Add(car1);
+            Car car2 = new Car() 
+            { 
+                EngineNoise = "Quiet", 
+                Model = "Corolla", 
+                HonkNoise = "LOUD", 
+                IsDrivable = true, 
+                Year = 2019, 
+                Make = "Camry" 
+            };
+            LottaCar.ListOfCars.Add(car2);
+
+            Car car3 = new Car("Toyota", 2019, false, "camry", "Medium", "Loud Enough");
+            LottaCar.ListOfCars.Add(car3);
+
+            foreach (Car item in LottaCar.ListOfCars)
+            {
+                Console.WriteLine($" Make: {car.Make}, Model: {car.Model}, Year: {car.Year}");
+            }
+
+
+
+
 
             //*************BONUS*************//
 
